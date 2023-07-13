a705f850ef28a3fbb394804a107fa24270aba7b9
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-07-13 11:37:02 +0200

    Create draft_base_line.py

---
+++ draft_base_line.py
@@ -0,0 +1,28 @@
+import uuid
+import datetime
+
+class BaseModel:
+    """ Defines all common attributes/methods for other classes """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+    
+    def __str__(self):
+        """
+        Returns the string representation of the current instance
+        """
+        return '{} {} {}'.format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates public instance attribute 'updated_at' with current datetime
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all key/values of '__dict__' of the
+        instance.
+        """
+        return self.__dict__
